7a2e76574bb0a8949071be404c203881edcc776c
raisilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2022-09-19 12:06:43 +0700

    Register

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,48 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:main_app/screens/register_screen.dart';
 import 'package:main_app/utils/colors.dart';
 import 'package:main_app/widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    _emailController.dispose();
-    _passwordController.dispose();
-  }
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _usernameController = TextEditingController();
+
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _passwordController2 = TextEditingController();
+
     return Scaffold(
       // appBar: AppBar(
       //   title: const Text('Scriptgram'),
       // ),
       body: SafeArea(
-          child: Container(
+        child: Container(
+          
         padding: const EdgeInsets.symmetric(horizontal: 32),
         width: double.infinity,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Flexible(
-              child: Container(),
-              flex: 2,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+                Container(
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  child: const Text(
+                    "Please Fill your information",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ),
+              ],
             ),
+            const SizedBox(height: 30),
             // svg image
-            Image.asset(
-              'assets/ic_scriptgram.png',
-              height: 64,),
-            const SizedBox(height: 64),
+            // Image.asset(
+            //   'assets/ic_scriptgram.png',
+            //   height: 64,),
+            TextFieldInput(
+              hintText: 'Enter your Username',
+              textInputType: TextInputType.emailAddress,
+              textEditingController: _usernameController,
+            ),
+
+            const SizedBox(height: 24),
             // text field input for email
             TextFieldInput(
               hintText: 'Enter your email',
@@ -62,10 +75,19 @@ class _LoginScreenState extends State<LoginScreen> {
             const SizedBox(
               height: 24,
             ),
+            TextFieldInput(
+              hintText: 'Repeat your password',
+              textInputType: TextInputType.text,
+              textEditingController: _passwordController2,
+              isPass: true,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
             // button login
             InkWell(
               child: Container(
-                child: const Text('Log in'),
+                child: const Text('Register'),
                 width: double.infinity,
                 alignment: Alignment.center,
                 padding: const EdgeInsets.symmetric(vertical: 12),
@@ -84,32 +106,6 @@ class _LoginScreenState extends State<LoginScreen> {
             Flexible(child: Container(), flex: 2),
 
             // Transitioning to signing up
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: const Text("Don't have an account ? "),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 20,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterScreen()));
-                  },
-                  child: Container(
-                    child: const Text(
-                      "Sign Up",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8,
-                    ),
-                  ),
-                ),
-              ],
-            )
           ],
         ),
       )),
